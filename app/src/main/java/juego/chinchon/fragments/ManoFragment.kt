f8b1f07a2dc8c8550362addf34931613b5c5cf60
LeonMarchetti <mleonardoa@gmail.com>
2020-03-16 18:01:38 -0300

    Arreglé advertencia de "useless layout"

--- app/src/main/java/juego/chinchon/fragments/ManoFragment.kt
+++ app/src/main/java/juego/chinchon/fragments/ManoFragment.kt
@@ -2,7 +2,6 @@ package juego.chinchon.fragments
 
 import android.os.Bundle
 import android.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -54,7 +53,6 @@ class ManoFragment : Fragment() {
                 }
             }
         }
-
         return v
     }
 
